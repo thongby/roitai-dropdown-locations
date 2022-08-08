6d85ada9b67be5822ced1db21f3dee8ceeec6542
Thong Boonyot <thongby@gmail.com>
2022-08-08 11:16:24 +0700

    Client side-Get label from dropdown list: Changwat amphoe tambon

--- client/src/components/DropdownPages.js
+++ client/src/components/DropdownPages.js
@@ -7,6 +7,14 @@ const DropdownPages = () => {
   const [amphoe, setAmphoe] = useState([]);
   const [tambon, setTambon] = useState([]);
 
+  const [data, setData] = useState({
+    title: "",
+    address: "",
+    province: "",
+    amphoe: "",
+    tambon: "",
+  });
+
   useEffect(() => {
     loadData();
   }, []);
@@ -21,8 +29,16 @@ const DropdownPages = () => {
       });
   };
 
-  const onChangeProvince = (id) => {
-    listAmphoe(id)
+  const onChangeData = (e) => {
+    setData({ ...data,[e.target.name]:e.target.value})
+  }
+
+  const onChangeProvince = (e) => {
+    let index = e.nativeEvent.target.selectedIndex;
+    let label = e.nativeEvent.target[index].text;
+    setData({...data,[e.target.name]:label});
+
+    listAmphoe(e.target.value)
       .then((res) => {
         setAmphoe(res.data);
       })
@@ -31,8 +47,12 @@ const DropdownPages = () => {
       });
   };
 
-  const onChangeAmphoe = (id) => {
-    listTambon(id)
+  const onChangeAmphoe = (e) => {
+    let index = e.nativeEvent.target.selectedIndex;
+    let label = e.nativeEvent.target[index].text;
+    setData({...data,[e.target.name]:label});
+
+    listTambon(e.target.value)
       .then((res) => {
         setTambon(res.data);
       })
@@ -41,17 +61,21 @@ const DropdownPages = () => {
       });
   };
 
-  const onChangeTambon = (id) => {
-    console.log(id);
+  const onChangeTambon = (e) => {
+    let index = e.nativeEvent.target.selectedIndex;
+    let label = e.nativeEvent.target[index].text;
+    setData({...data,[e.target.name]:label});
   };
 
-  console.log(tambon);
+  console.log(data);
 
   return (
     <>
       <h1>DropdownPages</h1>
       <form>
-        <select onChange={(e) => onChangeProvince(e.target.value)}>
+        <input type="text" name="title" onChange={(e)=>onChangeData(e)} />
+        <input type="text" name="address" onChange={(e)=>onChangeData(e)}/>
+        <select name="province" onChange={(e) => onChangeProvince(e)}>
           {province.map((item, index) => (
             <option key={index} value={item.ch_id}>
               {item.changwat_t}
@@ -59,7 +83,7 @@ const DropdownPages = () => {
           ))}
         </select>
 
-        <select onChange={(e) => onChangeAmphoe(e.target.value)}>
+        <select name="amphoe" onChange={(e) => onChangeAmphoe(e)}>
           {amphoe.map((item, index) => (
             <option key={index} value={item.am_id}>
               {item.amphoe_t}
@@ -67,15 +91,13 @@ const DropdownPages = () => {
           ))}
         </select>
 
-        <select onChange={(e) => onChangeTambon(e.target.value)}>
+        <select name="tambon" onChange={(e) => onChangeTambon(e)}>
           {tambon.map((item, index) => (
             <option key={index} value={item.ta_id}>
               {item.tambon_t}
             </option>
           ))}
         </select>
-
-        
       </form>
     </>
   );
